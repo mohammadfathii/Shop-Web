a4230ce05c1836c4a75988b750215256c688f43d
Mohammad <m_fathi65@yahoo.com>
2023-04-29 01:17:29 +0430

    update dbcontext in program file

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext(builder.Services.UseSql());
+builder.Services.AddDbContext<DbContext>(options => options.UseSql(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
